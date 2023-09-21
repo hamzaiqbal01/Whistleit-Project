3799133d9c262295cc53dfc553e9375f5b018f23
Salman-Irfan <salmanirfan674@gmail.com>
2023-09-21 16:29:45 +0500

    updated comment in the usersApi

--- Whistleit/src/apiIntegrations/adminPanelApis/usersApis.js
+++ Whistleit/src/apiIntegrations/adminPanelApis/usersApis.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://10.0.10.112:8000";
 
 export default class UserApis {
-  // 1. to get all the posts from the database
+  // 1. to get all the users from the database
   static async getAllUsers() {
     const res = await axios.get(`${BASE_URL}/show`);
     return res.data;
